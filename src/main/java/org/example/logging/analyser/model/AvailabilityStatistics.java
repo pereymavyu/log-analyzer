947cf7b1bc8e9b96ec6a9915fe963b8c27c30f87
pereymavu <pereyma.ic@gmail.com>
2024-07-26 08:43:47 +0700

    revert unnecessary fix

--- src/main/java/org/example/logging/analyser/model/AvailabilityStatistics.java
+++ src/main/java/org/example/logging/analyser/model/AvailabilityStatistics.java
@@ -1,14 +1,12 @@
 package org.example.logging.analyser.model;
 
 import lombok.Getter;
-import lombok.NonNull;
 
 @Getter
 public class AvailabilityStatistics {
     private long successRequestCount;
     private long failedRequestCount;
 
-    @NonNull
     public void addLogRecord(LogRecord logRecord) {
         if (logRecord.getIsSuccessful()) {
             ++successRequestCount;
